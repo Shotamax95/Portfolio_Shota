0e4a8b87d957de5909f9911076e5991e433a7b2b
Shotamax95 <shota.110.free.style@gmail.com>
2021-05-20 21:12:17 -0400

    draft idea

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,41 @@
 // Custom JavaScript
+
+// (function(){
+
+//     function Start(){
+//         console.log(`%c App Started...`,
+//         "font-size: 20px; color: blue; font-weight: bold"); 
+//     }
+
+//     window.addEventListener("load", Start);
+// })();
+
+const text = document.querySelector('.name'); // to get name
+const strText = text.textContent;
+const splitText = strText.split(""); // split my name 
+text.textContent =""; // reset name
+
+// asign span for each char
+for(let i = 0; i < splitText.length; i++)
+{
+    text.innerHTML +="<span>"+ splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char]; // to get span (all letters)
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length)
+    {
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('content/about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('content/projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/skills', function(req, res, next) {
+  res.render('content/skills', { title: 'Skills' });
 });
 
 module.exports = router;
